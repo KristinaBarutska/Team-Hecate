6116a4858d7c529107549930d0495895a7938cab
ssgivanova <ssgivanova@gmail.com>
2016-06-16 17:43:44 +0300

    added joker classes

---
+++ HecateMillionaire/HecateMillionaire/Joker/CallFriendJoker.cs
@@ -0,0 +1,14 @@
+﻿namespace HecateMillionaire.Jokers
+{
+    class CallFriendJoker : Joker
+    {
+
+        public override void UseJoker()
+        {
+            //create a Friend 
+            //generate his answer - he knows the answer, he is not sure, he don't know the answer
+            //mark the joker as used
+            this.IsUsed = true;
+        }
+    }
+}
