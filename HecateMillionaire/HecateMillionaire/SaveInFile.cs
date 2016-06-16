49ef23a946a6db2e9ca96d41a47c58cb347c1a30
Vekoks <veselin.k.krustev@gmail.com>
2016-06-16 15:35:33 +0300

    Save in text file.

---
+++ HecateMillionaire/HecateMillionaire/SaveInFile.cs
@@ -0,0 +1,37 @@
+﻿namespace HecateMillionaire
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+
+    public static class SaveInFile
+    {
+        public static void SetFileRekord(int scorePlayer, string namePlayer) //Save on record
+        {
+
+            string path = @"..\..\Record.txt";
+
+            FileStream CheckFile;
+
+            //Check file for existence
+            if (!File.Exists(@"..\..\Record.txt"))
+            {
+                CheckFile = File.Create(@"..\..\Record.txt");
+                CheckFile.Close();
+            }
+            else
+            {
+                //Write in file
+
+                string UpdateScore = scorePlayer.ToString() + " by " + namePlayer;
+
+                using (StreamWriter file = new StreamWriter(path, true)) //true -> save without clear file
+                {
+                    file.WriteLine(UpdateScore);
+                }
+            }
+        }
+    }
+}
