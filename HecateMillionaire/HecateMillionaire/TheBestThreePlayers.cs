3b0ee560f14c4a69f4b16e131a44a7cfb1572b52
Vekoks <veselin.k.krustev@gmail.com>
2016-06-16 15:52:46 +0300

    Read from file and print three player with best score.

--- HecateMillionaire/HecateMillionaire/TheBestThreePlayers.cs
+++ HecateMillionaire/HecateMillionaire/TheBestThreePlayers.cs
@@ -4,10 +4,89 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
 
-    class TheBestThreePlayers
+    public static class TheBestThreePlayers
     {
         //ToDo: print top three players with the best score and set background-color to see where is the player
+        public static void Show(string nameOfPlayer)
+        {
+            var records = ReadFromFile.GetFileRecord();
+            int[] sortNumber = new int[records.Length];
+
+            int count = 0;
+
+            for (int j = 0; j < records.Length; j++)
+            {
+                if (records[j] != null)
+                {
+                    sortNumber[count] = int.Parse(records[j].Substring(0, 4)); //take 4 digits
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            string TempString; //for sorting strin arr
+            int TempInt; //for sorting int arr
+
+            //Sort record
+            for (int j = 0; j < records.Length; j++)
+            {
+                for (int k = 0; k < records.Length; k++)
+                {
+
+                    if (sortNumber[j] > sortNumber[k])
+                    {
+                        TempString = records[j];
+                        records[j] = records[k];
+                        records[k] = TempString;
+
+                        TempInt = sortNumber[j];
+                        sortNumber[j] = sortNumber[k];
+                        sortNumber[k] = TempInt;
+                    }
+
+                }
+            }
+            //
+
+            Console.WriteLine("Standing:");
+
+            //Print only three record
+            for (int i = 0; i < records.Length; i++)
+            {
+                if (records[i] != null)
+                {
+                    if (i < 3)
+                    {
+                        Console.WriteLine(i + 1 + " -> " + records[i]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            //
+
+            //Print player position
+            Console.WriteLine("\nYour position is: ");
+
+            for (int i = 0; i < records.Length; i++)
+            {
+                var currnet = records[i].Split(' ');
+                if (nameOfPlayer == currnet[2])
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(i + 1 + " -> " + records[i]);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                }
+            }
+            //
+
+        }
     }
 }
