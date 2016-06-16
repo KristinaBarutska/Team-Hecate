694ae4279c55a96e2cb7100f403eeb61729c73cf
KristinaBarutska <KristinaBarutska@users.noreply.github.com>
2016-06-16 22:37:29 +0300

    Add files via upload

---
+++ MakeQuestions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hecate
+{
+    class MakeQuestions
+    {
+        public Question[] questColection =
+                {
+                new Question("Къде е роден Тодор Живков?", "Варна", "Правец", "Москва", "Истамбул","B"),
+                new Question("Коя е столицата на Монголия?", "Банкок", "Куала Лумпур", "Улан Батор", "Бишкек","C"),
+
+            };
+        public void DisplayQuestCollection()
+        {
+            foreach (var question in questColection)
+            {
+                Console.WriteLine(question.ToString());
+            }         
+        }
+    }
+}
