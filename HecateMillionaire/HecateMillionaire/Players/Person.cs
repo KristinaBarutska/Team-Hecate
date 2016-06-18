83e2a6d009ccc37379b6ba6679dd974e0d7a4718
ssgivanova <ssgivanova@gmail.com>
2016-06-18 13:44:46 +0300

    add abstract class Person and plan.txt

---
+++ HecateMillionaire/HecateMillionaire/Players/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace HecateMillionaire.Players
+{
+    using Common;
+    using System;
+
+    abstract class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                if (value.Length < 4)
+                {
+                    throw new ArgumentException(GlobalErrorMessages.InvalidPlayerNameErrorMessage);
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+    }
+}
