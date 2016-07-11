fc78add20cc28f69d5179506dc027ee87ef542ed
ssgivanova <ssgivanova@gmail.com>
2016-07-12 00:06:23 +0300

    small changes,extract interface IJoker,IQuestion

---
+++ HecateMillionaire/HecateMillionaire/Jokers/Contracts/IJoker.cs
@@ -0,0 +1,11 @@
+﻿namespace HecateMillionaire.Jokers.Contracts
+{
+    public interface IJoker
+    {
+        JokerType Type { get; set; }
+
+        bool IsUsed { get; set; }
+
+        void UseJoker();
+    }
+}
---
+++ HecateMillionaire/HecateMillionaire/Questions/Contracts/IQuestion.cs
@@ -0,0 +1,18 @@
+﻿namespace HecateMillionaire.Questions.Contracts
+{
+    public interface IQuestion
+    {
+        
+        int QuestionScore { get; set; }
+
+        int TimerSeconds { get; set; }
+
+        string[] Answers { get; }
+
+        string QuestionText { get;}
+
+        int RightAnswerIndex { get; set; }
+        
+        string PrintAnswers(bool flag);
+    }
+}
