a262407cc132f07a0fbf98a6cbde71ce0121da94
KristinaBarutska <KristinaBarutska@users.noreply.github.com>
2016-07-12 12:23:27 +0300

    Update ConsoleConstants.cs

--- HecateMillionaire/HecateMillionaire/Common/Console/ConsoleConstants.cs
+++ HecateMillionaire/HecateMillionaire/Common/Console/ConsoleConstants.cs
@@ -9,17 +9,17 @@
         public const string WrongAnswerMessage = "Sorry, You are wrong.";
         public const string ThreeWrongAnswersMessage = "You have 3 wrong answers !";
         public const string JokersMessage = "Jokers:";
-        public const string NextQuestionMessage = "Next question :";
+        public const string NextQuestionMessage = "Next question:";
         public const string StandingMessage = "Standing:";
         public const string PositionMessage = "Your position is: ";
         public const string TimeForAnswerMessage = "You have 1 minute to answer the question!";
-        public const string LastQuestionMessage = "That was the last question.You can't skip it.";
-        public const string BonusJokerMessage = "BONUS!You unlock the jokers!";
+        public const string LastQuestionMessage = "That was the last question. You can't skip it.";
+        public const string BonusJokerMessage = "BONUS! You unlock the jokers!";
         public const string NoJokerMessage = "You don't have jokers anymore!";
-        public const string BonusSkippedMessage = "BONUS!You can skip one question!";
-        public const string LostTextMessage = "Sorry.You`re lost, but you can try another game.Don`t you worry :)";
-        public const string PublicVoteMessage = "Public thing: ";
-        public const string CallFriendMessage = "Who friend you want to call!";
+        public const string BonusSkippedMessage = "BONUS! You can skip one question!";
+        public const string LostTextMessage = "Sorry. You`re lost, but you can try another game. Don`t you worry :)";
+        public const string PublicVoteMessage = "Public think: ";
+        public const string CallFriendMessage = "Who do you want to call?";
         public const string SkipeMessage = "Do you want to skip this question? ==> Press 'Y' for yes or 'N' for no :";
         public const string InvalidSkipChoice = "Invalid choice for skipping";
     }
